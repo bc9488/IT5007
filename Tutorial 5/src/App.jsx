249669d869286fc5680a2b6ef64f97c0f99e6c2a
bc9488 <bc9488@yahoo.com.sg>
2021-10-27 20:15:06 +0800

    update

---
+++ Tutorial 5/src/App.jsx
@@ -0,0 +1,187 @@
+const dateRegex = new RegExp('^\\d\\d\\d\\d-\\d\\d-\\d\\d');
+
+function jsonDateReviver(key, value) {
+  if (dateRegex.test(value)) return new Date(value);
+  return value;
+}
+
+class IssueFilter extends React.Component {
+  render() {
+    
+      return (
+        <div></div>
+    );
+  }
+}
+
+function IssueRow(props) {
+  const issue = props.issue;
+  return (
+    <tr>
+      <td>{issue.id}</td>
+      <td>{issue.customerName}</td>
+      <td>{issue.phoneNum}</td>
+      <td>{issue.created.toDateString()}</td>
+    </tr>
+  );
+}
+
+function IssueTable(props) {
+  const issueRows = props.issues.map(issue =>
+    <IssueRow key={issue.id} issue={issue} />
+  );
+
+  return (
+    <table className="bordered-table">
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Customer Name</th>
+          <th>Phone Number</th>
+          <th>Time Stamp</th>
+        </tr>
+      </thead>
+      <tbody>
+        {issueRows}
+      </tbody>
+    </table>
+  );
+}
+
+class IssueAdd extends React.Component {
+  constructor() {
+    super();
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const form = document.forms.issueAdd;
+    const issue = {
+      customerName: form.customerName.value, phoneNum: form.phoneNum.value,
+      
+    }
+    this.props.createIssue(issue);
+    form.customerName.value = ""; form.phoneNum.value = "";
+  }
+
+  render() {
+    return (
+      <form name="issueAdd" onSubmit={this.handleSubmit}>
+        <input type="text" name="customerName" placeholder="Customer Name" />
+        <input type="number" name="phoneNum" placeholder="Phone Number" />
+        <button>Add</button>
+      </form>
+    );
+  }
+}
+
+class IssueDelete extends React.Component {
+  constructor() {
+    super();
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const form = document.forms.issueAdd;
+    const issue = {
+      customerName: form.customerName.value, phoneNum: form.phoneNum.value,
+      
+    }
+    form.customerName.value = ""; form.phoneNum.value = "";
+  }
+
+  render() {
+    return (
+      <form name="issueDelete" onSubmit={this.handleSubmit}>
+        <input type="text" name="customerName" placeholder="Customer Name" />
+        <input type="number" name="phoneNum" placeholder="Phone Number" />
+        <button>Delete</button>
+      </form>
+    );
+  }
+}
+
+async function graphQLFetch(query, variables = {}) {
+  try {
+    const response = await fetch('/graphql', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json'},
+      body: JSON.stringify({ query, variables })
+    });
+    const body = await response.text();
+    const result = JSON.parse(body, jsonDateReviver);
+
+    if (result.errors) {
+      const error = result.errors[0];
+      if (error.extensions.code == 'BAD_USER_INPUT') {
+        const details = error.extensions.exception.errors.join('\n ');
+        alert(`${error.message}:\n ${details}`);
+      } else {
+        alert(`${error.extensions.code}: ${error.message}`);
+      }
+    }
+    return result.data;
+  } catch (e) {
+    alert(`Error in sending data to server: ${e.message}`);
+  }
+}
+
+class IssueList extends React.Component {
+  constructor() {
+    super();
+    this.state = { issues: [] };
+    this.createIssue = this.createIssue.bind(this);
+  }
+
+  componentDidMount() {
+    this.loadData();
+  }
+
+  async loadData() {
+    const query = `query {
+      issueList {
+        id customerName phoneNum
+        created
+      }
+    }`;
+
+    const data = await graphQLFetch(query);
+    if (data) {
+      this.setState({ issues: data.issueList });
+    }
+  }
+
+  async createIssue(issue) {
+    const query = `mutation issueAdd($issue: IssueInputs!) {
+      issueAdd(issue: $issue) {
+        id
+      }
+    }`;
+
+    const data = await graphQLFetch(query, { issue });
+    if (data) {
+      this.loadData();
+    }
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <h1>WaitList</h1>
+        <IssueFilter />
+        <hr />
+        <IssueTable issues={this.state.issues} />
+        <hr />
+        <IssueAdd createIssue={this.createIssue} />
+        <hr />
+        <IssueDelete />
+      </React.Fragment>
+    );
+  }
+}
+
+const element = <IssueList />;
+
+ReactDOM.render(element, document.getElementById('contents'));
