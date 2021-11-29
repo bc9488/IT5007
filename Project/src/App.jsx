9f73434e27693e44d6d208e1ae553ecc0d869b7c
bc9488 <bc9488@yahoo.com.sg>
2021-11-29 21:41:42 +0800

    update

--- Project/src/App.jsx
+++ Project/src/App.jsx
@@ -1,3 +1,21 @@
+function openContent(evt, contName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(contName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+document.getElementById("defaultOpen").click();
+
+
+/*
 const initialIssues = [
     {
       id: 1, customerName: 'Ravan', phoneNumber: '12345678',
@@ -165,22 +183,10 @@ const initialIssues = [
     }
 
   }
-  
-  function openContent(evt, contName) {
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-    document.getElementById(contName).style.display = "block";
-    evt.currentTarget.className += " active";
-  }
-  const element = <IssueList />;
-  
-  document.getElementById("defaultOpen").click();
+
   ReactDOM.render(element, document.getElementById('contents'));
- 
+  */
+
+
+  
+
